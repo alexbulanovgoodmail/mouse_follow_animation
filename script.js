87c841d909d76ed064e1a42679c2a2f18d82d45a
Александр Буланов <alexbulanovgoodmail@gmail.com>
2026-01-14 22:21:17 +0300

    Вывод координат на экране

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let mouseX = 0;
+  let mouseY = 0;
+
+  const coordinates = document.querySelector("#coordinates strong");
+
+  window.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    if (coordinates) {
+      coordinates.innerHTML = `x: ${mouseX} y: ${mouseY}`;
+    }
+  });
+});
