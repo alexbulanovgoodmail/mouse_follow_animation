cdf0b7c249372870e39307898d7ee1edb8539c4a
Александр Буланов <alexbulanovgoodmail@gmail.com>
2026-01-14 22:42:21 +0300

    Обновлена разметка, стили, добавлен скрипт анимации указателя

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let mouseX = 0;
-  let mouseY = 0;
+  const isReducedMotion = window.matchMedia(
+    "(prefers-reduced-motion: reduce)"
+  ).matches;
+  const isTouch = window.matchMedia("(pointer: coarse)").matches;
+  const shouldRun = !isReducedMotion && !isTouch;
 
-  const coordinates = document.querySelector("#coordinates strong");
+  if (shouldRun) {
+    let mouseX = 0;
+    let mouseY = 0;
 
-  window.addEventListener("mousemove", (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+    const coordinates = document.querySelector("#coordinates strong");
+    const pointer = document.querySelector(".pointer");
 
-    if (coordinates) {
-      coordinates.innerHTML = `x: ${mouseX} y: ${mouseY}`;
+    window.addEventListener("mousemove", (e) => {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+
+      if (coordinates) {
+        coordinates.innerHTML = `x: ${mouseX} y: ${mouseY}`;
+      }
+
+      if (pointer) {
+      }
+    });
+
+    function animate() {
+      pointer.style.setProperty("--mouseX", `${mouseX}px`);
+      pointer.style.setProperty("--mouseY", `${mouseY}px`);
+
+      requestAnimationFrame(animate);
     }
-  });
+
+    animate();
+  }
 });
